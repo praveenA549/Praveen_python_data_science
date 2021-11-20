a7f5ef3224ca49f1a8e97e77cf993eed2a986873
praveen <pocha.praveen549@gmail.com>
2021-11-20 12:21:30 +0530

    fisrt program added

---
+++ Fisrt_PY_program.py
@@ -0,0 +1,7 @@
+first_name = input("Enter first name: ")
+second_name = input("Ener second name: ")
+def hello(fn,sn):
+    sen = "Hi, Your full name is "
+    fullsen = sen+fn+' '+sn
+    print(fullsen)
+hello(first_name,second_name)
